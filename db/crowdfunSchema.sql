f25d7b2502adaf703d3a9a3a03a3ef6d6994d986
Alex Foo <alexfoodw@gmail.com>
2018-10-07 22:04:22 +0800

    complete entity tables - user, project and admin

--- db/crowdfunSchema.sql
+++ db/crowdfunSchema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE admin (
 first_name  VARCHAR(32),
 last_name  VARCHAR(32),
 password  VARCHAR(32),
@@ -6,7 +6,18 @@ email  VARCHAR(128),
 PRIMARY KEY (email)
 );
 
-CREATE TABLE project(
+CREATE TABLE user (
+first_name    VARCHAR(32) NOT NULL,
+last_name     VARCHAR(32) NOT NULL,
+password      VARCHAR(6) NOT NULL,
+email         VARCHAR(64) NOT NULL PRIMARY KEY,
+dob           DATE  NOT NULL,
+since         DATE  NOT NULL,
+birth_country VARCHAR(32) NOT NULL,
+phone         VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE project (
 title             VARCHAR(128) NOT NULL,
 description       VARCHAR(256) NOT NULL,
 project_id        VARCHAR(32) PRIMARY KEY,
@@ -26,28 +37,18 @@ source_url        VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE creates (
-date  DATE,
-u_email  VARCHAR(128),
+u_email  VARCHAR(64),
 p_projectID  VARCHAR(32) PRIMARY KEY,
 FOREIGN KEY (u_email) REFERENCES user (email),
 FOREIGN KEY (p_projectID) REFERENCES project (project_id)
 );
 
 CREATE TABLE funds (
-date  DATE,
+fund_date  DATETIME,
 amount  INT,
-u_email  VARCHAR(128),
+u_email  VARCHAR(64),
 p_projectID  VARCHAR(32),
 PRIMARY KEY (u_email, p_projectID),
 FOREIGN KEY (u_email) REFERENCES user (email),
 FOREIGN KEY (p_projectID) REFERENCES project (project_id)		
-);
-
-
-CREATE TABLE admin (
-first_name  VARCHAR(32),
-last_name  VARCHAR(32),
-password  VARCHAR(32),
-email  VARCHAR(128),
-PRIMARY KEY (email)
 );
