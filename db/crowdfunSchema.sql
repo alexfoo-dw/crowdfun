83a45beecaa89109fafba4f571c871839168e40e
Sarah Ng <sarahnzx@gmail.com>
2018-10-08 23:23:49 +0800

    modify schema, userAdmin, project and creates

--- db/crowdfunSchema.sql
+++ db/crowdfunSchema.sql
@@ -6,7 +6,7 @@ email  VARCHAR(128),
 PRIMARY KEY (email)
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
 first_name    VARCHAR(32) NOT NULL,
 last_name     VARCHAR(32) NOT NULL,
 password      VARCHAR(6) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE project (
 title             VARCHAR(128) NOT NULL,
 description       VARCHAR(256) NOT NULL,
 project_id        VARCHAR(32) PRIMARY KEY,
-start_date        DATETIME  NOT NULL,
+start_date        DATE  NOT NULL,
 duration          INT  NOT NULL,
 keywords          VARCHAR(256) NOT NULL,
 amount_sought     INT  NOT NULL,
@@ -39,16 +39,16 @@ source_url        VARCHAR(128) NOT NULL
 CREATE TABLE creates (
 u_email  VARCHAR(64),
 p_projectID  VARCHAR(32) PRIMARY KEY,
-FOREIGN KEY (u_email) REFERENCES user (email),
+FOREIGN KEY (u_email) REFERENCES users (email),
 FOREIGN KEY (p_projectID) REFERENCES project (project_id)
 );
 
 CREATE TABLE funds (
-fund_date  DATETIME NOT NULL,
+fund_date  DATE NOT NULL,
 amount  INT NOT NULL,
 u_email  VARCHAR(64),
 p_projectID  VARCHAR(32),
 PRIMARY KEY (u_email, p_projectID),
-FOREIGN KEY (u_email) REFERENCES user (email),
+FOREIGN KEY (u_email) REFERENCES users (email),
 FOREIGN KEY (p_projectID) REFERENCES project (project_id)		
 );
