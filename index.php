1c5d356e6b945c2709df53ddbd680af239ca39fb
Sarah Ng <sarahnzx@gmail.com>
2018-10-09 12:15:56 +0800

    implement search for project and users

--- index.php
+++ index.php
@@ -8,21 +8,21 @@
   <h2>Crowdfun</h2>
   <ul>
     <form name="display" action="index.php" method="POST" >
-      <li>Book ID:</li>
+      <li>Search project category:</li>
       <li><input type="text" name="bookid" /></li>
-      <li><input type="submit" name="submit" /></li>
+      <li><input type="submit" name="project" value = "Search"/></li>
     </form>
   </ul>
   <?php
   	// Connect to the database. Please change the password in the following line accordingly
     $db     = pg_connect("host=localhost port=5432 dbname=crowdfun user=postgres password=password");	
     // $result = pg_query($db, "SELECT * FROM book where book_id = '$_POST[bookid]'");		// Query template
-    $result = pg_query($db, "SELECT * FROM project;");
+    $result = pg_query($db, "SELECT * FROM project WHERE category = '$_POST[bookid]';");
     // $row    = pg_fetch_assoc($result);		// To store the result row
 
     // echo "$row['first_name']";
 
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['project'])) {
       $html = "<h1>Project table</h1><br>
       <table>
       <tr>
@@ -66,6 +66,69 @@
         
       }
       $html .= "</table>";
+      // $html .= "<h1>Users table</h1><br>
+      // <table>
+      // <tr>
+      // <th>first_name</th>
+      // <th>last_name</th>
+      // <th>password</th>
+      // <th>email</th>
+      // <th>dob</th>
+      // <th>since</th>
+      // <th>birth_country</th>
+      // <th>phone</th>
+      // </tr>";
+      // $html .= "</table>";
+
+
+
+      // <ul><form name='update' action='index.php' method='POST' >  
+      // <li>Title:</li>  
+      // <li><input type='text' name='bookid_updated' value='$row[title]' /></li>  
+      // <li>Book Name:</li>  
+      // <li><input type='text' name='book_name_updated' value='$row[name]' /></li>  
+      // <li>Price (USD):</li><li><input type='text' name='price_updated' value='$row[price]' /></li>  
+      // <li>Date of publication:</li>  
+      // <li><input type='text' name='dop_updated' value='$row[date_of_publication]' /></li>  
+      // <li><input type='submit' name='new' /></li>  
+      // </form>  
+      // </ul>";
+        echo $html;
+
+      // echo "$row['first_name']";
+    }
+    if (isset($_POST['new'])) {	// Submit the update SQL command
+        $result = pg_query($db, "UPDATE book SET book_id = '$_POST[bookid_updated]',  
+    name = '$_POST[book_name_updated]',price = '$_POST[price_updated]',  
+    date_of_publication = '$_POST[dop_updated]'");
+        if (!$result) {
+            echo "Update failed!!";
+        } else {
+            echo "Update successful!";
+        }
+    }
+
+      // echo "$row[0]";
+    ?>  
+
+    <ul>
+      <form name="display" action="index.php" method="POST" >
+        <li>Search user email:</li>
+        <li><input type="text" name="email" /></li>
+        <li><input type="submit" name="users" value = "Search"/></li>
+      </form>
+    </ul>
+  <?php
+    // Connect to the database. Please change the password in the following line accordingly
+    $db     = pg_connect("host=localhost port=5432 dbname=crowdfun user=postgres password=password"); 
+    // $result = pg_query($db, "SELECT * FROM book where book_id = '$_POST[bookid]'");    // Query template
+    $result = pg_query($db, "SELECT * FROM users WHERE users.email = '$_POST[email]';");
+    // $row    = pg_fetch_assoc($result);   // To store the result row
+
+    // echo "$row['first_name']";
+
+    if (isset($_POST['users'])) {
+      $html = "";
       $html .= "<h1>Users table</h1><br>
       <table>
       <tr>
@@ -79,7 +142,7 @@
       <th>phone</th>
       </tr>";
 
-      $result = pg_query($db, "SELECT * FROM users;");
+      // $result = pg_query($db, "SELECT * FROM users;");
 
       while ($row = pg_fetch_assoc($result)) {
         $html .= "<tr>
@@ -113,7 +176,7 @@
 
       // echo "$row['first_name']";
     }
-    if (isset($_POST['new'])) {	// Submit the update SQL command
+    if (isset($_POST['new'])) { // Submit the update SQL command
         $result = pg_query($db, "UPDATE book SET book_id = '$_POST[bookid_updated]',  
     name = '$_POST[book_name_updated]',price = '$_POST[price_updated]',  
     date_of_publication = '$_POST[dop_updated]'");
